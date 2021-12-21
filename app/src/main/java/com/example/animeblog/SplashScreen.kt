fc4bd6f84a776ffc30c741e2c5522a514ec47fbd
Anirudh Malhotra <96338545+Tota-Ota@users.noreply.github.com>
2021-12-21 18:59:48 +0530

    Added animation

--- app/src/main/java/com/example/animeblog/SplashScreen.kt
+++ app/src/main/java/com/example/animeblog/SplashScreen.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+
+    private val splashScreen = 4000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -17,10 +23,21 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        val topAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        val appLogo: ImageView = findViewById(R.id.appLogo)
+        val appName: TextView = findViewById(R.id.appName)
+        val slogan: TextView = findViewById(R.id.slogan)
+
+        appLogo.animation = topAnim
+        appName.animation = bottomAnim
+        slogan.animation = bottomAnim
+
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, splashScreen.toLong())
     }
 }
